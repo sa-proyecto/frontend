34197d8dcad0072b515e3d0798cb8e2422bfee0e
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-12-09 17:08:00 -0600

    test de registro proveedor

--- src/app/registerpage/registerpage.component.spec.ts
+++ src/app/registerpage/registerpage.component.spec.ts
@@ -31,4 +31,46 @@ describe('ModificarProveedorComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('formulario de proveedor invalido cuando esta vacio', () => {
+    expect(component.registerProveedorForm.valid).toBeFalsy();
+  });
+
+  it('validar formulario proveedor', () => {
+    component.registerProveedorForm.patchValue({
+      nombre_empresa: 'a',
+      email: 'a@a',
+      direccion: 'a',
+      contrasena: 'a',
+      contrasena_confirm: 'a'
+    });
+    expect(component.registerProveedorForm.valid).toBeTruthy();
+  });
+
+  it('validar formulario proveedor correo correcto', () => {
+    component.registerProveedorForm.patchValue({
+      nombre_empresa: 'a',
+      email: 'a',
+      direccion: 'a',
+      contrasena: 'a',
+      contrasena_confirm: 'a'
+    });
+    const email = component.registerProveedorForm.controls.email;
+    const errors = email.errors;
+    expect(errors.email).toBeTruthy();
+  });
+
+  it('validar formulario proveedor contrasena correcto', () => {
+    component.registerProveedorForm.patchValue({
+      nombre_empresa: 'a',
+      email: 'a',
+      direccion: 'a',
+      contrasena: 'a',
+      contrasena_confirm: 'b'
+    });
+    const contrasena_confirm = component.registerProveedorForm.controls.contrasena_confirm;
+    const errors = contrasena_confirm.errors;
+    console.log(errors);
+    expect(errors.mustMatch).toBeTruthy();
+  });
 });
