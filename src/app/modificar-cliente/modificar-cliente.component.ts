493a3466efe46d072ebf14bae4841c468c3689e6
Mario <oiramalli@gmail.com>
2020-12-09 07:57:01 -0600

    Modificar

---
+++ src/app/modificar-cliente/modificar-cliente.component.ts
@@ -0,0 +1,132 @@
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormBuilder, Validators } from '@angular/forms';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../api/auth.service';
+import { MustMatch } from '../must-mach.validator';
+
+@Component({
+  selector: 'app-modificar-cliente',
+  templateUrl: './modificar-cliente.component.html',
+  styleUrls: ['./modificar-cliente.component.scss']
+})
+export class ModificarClienteComponent implements OnInit {
+  private focus1: boolean;
+  private focus2: boolean;
+  private focus3: boolean;
+  private focus4: boolean;
+  private focus5: boolean;
+  private sumbitted = false;
+  clientDataForm: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  get Form(): { [key: string]: AbstractControl } {
+    return this.clientDataForm.controls;
+  }
+
+  get Focus1(): boolean {
+    return this.focus1;
+  }
+
+  set Focus1(val: boolean) {
+    this.focus1 = val;
+  }
+
+  set Focus2(val: boolean) {
+    this.focus2 = val;
+  }
+
+  get Focus2(): boolean {
+    return this.focus2;
+  }
+
+  set Focus3(val: boolean) {
+    this.focus3 = val;
+  }
+
+  get Focus3(): boolean {
+    return this.focus3;
+  }
+
+  set Focus4(val: boolean) {
+    this.focus4 = val;
+  }
+
+  get Focus4(): boolean {
+    return this.focus4;
+  }
+
+  get Focus5(): boolean {
+    return this.focus5;
+  }
+
+  set Focus5(val: boolean) {
+    this.focus5 = val;
+  }
+
+  get Submitted(): boolean {
+    return this.sumbitted;
+  }
+
+  ngOnInit(): void {
+    this.clientDataForm = this.formBuilder.group({
+      nombre: ['',
+        Validators.compose([
+          Validators.required,
+        ]),
+      ],
+      apellido: ['',
+        Validators.compose([
+          Validators.required,
+        ]),
+      ],
+      celular: ['',
+        Validators.compose([
+          Validators.required,
+          Validators.max(8)
+        ]),
+      ],
+      email: ['',
+        Validators.compose([
+          Validators.required,
+          Validators.email,
+        ]),
+      ],
+      contrasena: ['',
+        Validators.compose([
+          Validators.required,
+        ]),
+      ],
+      contrasena_confirm: ['',
+        Validators.compose([
+          Validators.required,
+        ]),
+      ],
+    }, {
+      validator: MustMatch('contrasena', 'contrasena_confirm'),
+    });
+  }
+
+  submit() {
+    this.sumbitted = true;
+    if (!this.clientDataForm.valid) {
+      return;
+    }
+    // this.authService.clienteUpdate(this.clientDataForm.value)
+    // .subscribe((res) => {
+    //   if (res.status === 'success') {
+    //     this.router.navigate(['']); // Ir al inicio
+    //   } else {
+    //     // Mostrar Error
+    //   }
+    // }, (err) => {
+    //   // Mostrar Error
+    // });
+  }
+
+}
---
+++ src/app/modificar-proveedor/modificar-proveedor.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ModificarProveedorComponent } from './modificar-proveedor.component';
+
+describe('ModificarProveedorComponent', () => {
+  let component: ModificarProveedorComponent;
+  let fixture: ComponentFixture<ModificarProveedorComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ ModificarProveedorComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ModificarProveedorComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
