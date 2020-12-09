2e71dcc8585696a57b289e42945873d680607973
Guillermo Medinilla <medinilla.guillermo@gmail.com>
2020-12-09 12:06:00 -0600

    tests arreglados

--- src/app/api/auth.service.spec.ts
+++ src/app/api/auth.service.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { AuthService } from './auth.service';
 
@@ -6,11 +9,18 @@ describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        ReactiveFormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([]),
+
+      ],
+    });
     service = TestBed.inject(AuthService);
   });
 
-  // it('should be created', () => {
-  //   expect(service).toBeTruthy();
-  // });
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
 });
